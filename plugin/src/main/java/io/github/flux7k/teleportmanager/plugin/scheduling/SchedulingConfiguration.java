69ee5815cb1152350f0f91e4177e199f9284c967
flux7k <flux7k0@gmail.com>
2025-10-17 10:36:32 +0900

    Enable scheduling in SchedulingConfiguration

--- plugin/src/main/java/io/github/flux7k/teleportmanager/plugin/scheduling/SchedulingConfiguration.java
+++ plugin/src/main/java/io/github/flux7k/teleportmanager/plugin/scheduling/SchedulingConfiguration.java
@@ -4,11 +4,13 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
 import java.util.concurrent.Executor;
 
+@EnableScheduling
 @Configuration
 public class SchedulingConfiguration implements SchedulingConfigurer {
 
